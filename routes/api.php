17bb7f97b5253935af657f82a86c3309572b74a7
ghulam <ghulamalfakhri@gmail.com>
2021-11-23 23:35:23 +0700

    REST API CRUD

--- routes/api.php
+++ routes/api.php
@@ -16,24 +16,19 @@ use App\Http\Controllers\API\ProgramController;
 |
 */
 
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
 //API route for register new user
 Route::post('/register', [AuthController::class, 'register']);
-
 //API route for login user
 Route::post('/login', [AuthController::class, 'login']);
-
 //Protecting Routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
 Route::get('/profile', function(Request $request) {
     return auth()->user();
 });
-
 Route::resource('programs', ProgramController::class);
-
 // API route for logout user
 Route::post('/logout', [AuthController::class, 'logout']);
-});
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
 });
